26f3754513b29609beea2673e437dfa6e5e65a75
AFukuma <fukuma.akihiko@idh-net.co.jp>
2019-08-14 12:10:07 +0900

    Init

--- CognitoDesktop/Form1.Designer.cs
+++ CognitoDesktop/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(93, 11);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 19);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(93, 37);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(100, 19);
+            this.txtPass.TabIndex = 1;
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.Location = new System.Drawing.Point(93, 63);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(75, 23);
+            this.btnSignIn.TabIndex = 2;
+            this.btnSignIn.Text = "SignIn";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(219, 92);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
