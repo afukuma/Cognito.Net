5871d8303a7a63d0424efeac743577ddfd9acb2e
AFukuma <fukuma.akihiko@idh-net.co.jp>
2019-08-14 12:22:11 +0900

    cache rest

---
+++ CognitoDesktop/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Amazon;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CognitoDesktop
+{
+    public partial class Form1 : Form
+    {
+
+        private string wwUserPoolID = "";
+        private string wwAppClientID = "";
+        RegionEndpoint wwRegion;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //App.Configから各種情報の取得
+            wwUserPoolID = ConfigurationManager.AppSettings["UserPoolID"];
+            wwAppClientID = ConfigurationManager.AppSettings["AppClientID"];
+
+            string wwRegionString = ConfigurationManager.AppSettings["RegionString"];
+            wwRegion = null;
+            foreach(RegionEndpoint wR in RegionEndpoint.EnumerableAllRegions)
+            {
+                if (wR.SystemName == wwRegionString)
+                {
+                    wwRegion = wR;
+                    return;
+                }
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# CognitoDesktop
+
+
+App.ConfigÇ…â∫ãLÇÃÇÊÇ§Ç…ê›íËÇ∑ÇÈ
+
+  <appSettings>
+    <add key="UserPoolID" value="us-east-1_hhogehogeh" />
+    <add key="AppClientID" value="4ks0na9rghogehogehoge0l" />
+    <add key="RegionString" value="us-east-1" />
+    <add key="" value="" />
+  </appSettings>
+  
+| | | 
+| ----- | ------ |
+| | | 
+
+
